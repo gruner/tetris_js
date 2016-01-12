da9722afedd9ab2ad4c525744cf8c99701e37fdf
Andrew Gruner <andrew.gruner@gmail.com>
2016-01-11 16:13:24 -0800

    Implement AnimationQueue, add documentation

--- js/view/canvas.js
+++ js/view/canvas.js
@@ -4,12 +4,14 @@ var eventDispatcher = require('../eventDispatcher'),
     events = require('../config/events'),
     dimensions = require('../config/canvasDimensions'),
     Sprite = require('./sprite'),
+    RowCompleteAnimation = require('./rowCompleteAnimation'),
+    RowCollapseAnimation = require('./rowCollapseAnimation'),
     canvasCache = require('./canvasCache'),
     animationQueue = require('./animationQueue');
 
 var Canvas = function(canvasElement, gameEngine) {
 
-    this.ctx = null;
+    this.ctx = null; // canvas context
     this.gameEngine = gameEngine;
     this.imageCache = {};
 
@@ -34,41 +36,63 @@ Canvas.prototype.bindEvents = function() {
     eventDispatcher.subscribe(events.rowComplete, this.animateRowComplete);
 };
 
+/**
+ * Draws entire game on each update loop
+ */
 Canvas.prototype.draw = function() {
     this.drawPlayfield();
-    this.drawTetromino(this.gameEngine.activeTetromino);
-    // this.drawBlocks();
-    this.drawGhostPiece();
-    //this.drawAnimations();
-    animationQueue.draw();
+    this.drawBlocks();
+
+    // If animations are in-progress, draw the next frame,
+    // otherwise resume normal drawing
+    if (!animationQueue.draw()) {
+        this.drawGhostPiece(); // has to go before tetromino, so that it's behind it
+        this.drawTetromino(this.gameEngine.activeTetromino);
+    }
 };
 
+/**
+ * Draws the playfield - the background rectangle
+ * @TODO cache this as an image after the first draw
+ */
 Canvas.prototype.drawPlayfield = function() {
     var width = dimensions.transpose(this.gameEngine.playfield.xCount),
         height = dimensions.transpose(this.gameEngine.playfield.yCount);
 
     this.ctx.fillStyle = this.gameEngine.theme.playfield.color;
-    this.ctx.fillRect (dimensions.playfield.x, dimensions.playfield.y, width, height);
+    this.ctx.fillRect(dimensions.playfield.x, dimensions.playfield.y, width, height);
 };
 
+/**
+ * Draws all blocks (pieces of previously dropped tetrominos)
+ */
 Canvas.prototype.drawBlocks = function() {
+    var self = this;
+
     this.gameEngine.playfield.traverseGrid(function(block) {
-        var color = this.gameEngine.theme.playfield.color;
         if (typeof block !== 'undefined') {
-            color = block.color;
+            self.drawBlock(dimensions.transpose(block.x), dimensions.transpose(block.y), block.color);
         }
-        this.drawBlock(block.x, block.y, color);
     });
 };
 
+/**
+ * Draws a single block
+ */
 Canvas.prototype.drawBlock = function(x, y, color) {
     this.ctx.fillStyle = color;
     this.ctx.fillRect(x, y, dimensions.gridSize, dimensions.gridSize);
 };
 
-Canvas.prototype.drawTetromino = function(tetromino) {
+/**
+ * Draws a tetromino in the current theme
+ * TODO: Draw the outline of the entire tetromino rather than
+ * all of its blocks to prevent gaps between blocks
+ */
+Canvas.prototype.drawTetromino = function(tetromino, color) {
+    color = color || this.gameEngine.theme.tetrominos[tetromino.type].color;
+    
     var self = this,
-        color = this.gameEngine.theme.tetrominos[tetromino.type].color,
         x, y
         ;
     
@@ -80,26 +104,12 @@ Canvas.prototype.drawTetromino = function(tetromino) {
     });
 };
 
+/**
+ * Draws the ghost piece
+ */
 Canvas.prototype.drawGhostPiece = function() {
-    // var blocks = this.gameEngine.getGhostPiece().getBlockCoordinates();
-    // for (var i = 0; i < blocks.length; i++) {
-    //     this.drawBlock(
-    //         dimensions.transpose(blocks[i].x),
-    //         dimensions.transpose(blocks[i].y),
-    //         this.gameEngine.theme.ghostPiece.color
-    //     );
-    // }
-
-    this.drawTetromino(this.gameEngine.getGhostPiece());
+    this.drawTetromino(this.gameEngine.getGhostPiece(), this.gameEngine.theme.ghostPiece.color);
 };
-
-// Canvas.prototype.drawAnimations = function() {
-//     for (var i = 0; i < this.animations.length; i++) {
-//         if (typeof this.animations[i].draw === 'function') {
-//             this.animations[i].draw();
-//         }
-//     }
-// };
 
 // Canvas.prototype.getTetrominoSprite = function() {
 //     if (typeof this.tetrominoSprite === 'undefined') {
@@ -109,15 +119,14 @@ Canvas.prototype.drawGhostPiece = function() {
 //     this.tetrominoSprite.y = dimensions.transpose(this.gameEngine.activeTetromino.y);
 // };
 
+/**
+ * Handler for the rowComplete event
+ * Adds row complete and collapse animations to the animation queue
+ */
 Canvas.prototype.animateRowComplete = function(data) {
     var rows = data.rows;
-
-    // this.animationQueue.push(new animations.rowComplete(this.ctx, rows));
-    // animate the row(s) turning white, then disappearing,
-
-    // this.animationQueue.push(new animations.rowCollapse(this.ctx, rows));
-    // spliceTop() - save state of everything above completed rows,
-    // animate the saved top moving back down
+    animationQueue.push(new RowCompleteAnimation(this.ctx, rows));
+    animationQueue.push(new RowCollapseAnimation(this.ctx, rows));
 };
 
 Canvas.prototype.spliceTop = function() {};
