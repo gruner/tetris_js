cc78c9e6cab68e5a117451e2505fe0934b39c322
Andrew Gruner <andrew.gruner@gmail.com>
2016-01-11 16:18:04 -0800

    Add methods Playfield.prototype.removeRows and Playfield.prototype.traverseRows

--- js/models/playfield.js
+++ js/models/playfield.js
@@ -2,6 +2,11 @@
 
 var debug = require('../debug');
 
+/**
+ * Models the state of the playfield game grid.
+ * A multidimensional array represents the x and y grid coordinates.
+ * Each cell in the grid can hold one Block model
+ */
 var Playfield = function(xCount, yCount) {
     this.xCount = xCount;
     this.yCount = yCount;
@@ -14,7 +19,7 @@ Playfield.defaults = {
 };
 
 /**
- * Returns a multidimensional array of null values
+ * Returns a multidimensional array of undefined values
  * in the dimensions of the grid
  */
 Playfield.prototype.buildGrid = function() {
@@ -22,10 +27,21 @@ Playfield.prototype.buildGrid = function() {
 };
 
 /**
- * Returns an array of null values for each column in a row
+ * Returns an array of undefined values for each column in a row
  */
 Playfield.prototype.createEmptyRow = function() {
     return new Array(this.xCount);
+};
+
+/**
+ * Executes a callback for each row in the grid, passing the contents
+ * of the row to the callback
+ */
+Playfield.prototype.traverseRows = function(callback) {
+    for (var i = 0; i < this.yCount; i++) {
+        var row = this.grid[i];
+        callback(i, row);
+    }
 };
 
 /**
@@ -41,6 +57,18 @@ Playfield.prototype.traverseGrid = function(callback) {
             }
             callback(cell);
         }
+    }
+};
+
+/**
+ * Removes an array of rows one at a time
+ */
+Playfield.prototype.removeRows = function(rows) {
+    var i,
+        iMax = rows.length;
+
+    for (i = 0; i < iMax; i++) {
+        this.removeRowAt(i);
     }
 };
 
@@ -61,10 +89,12 @@ Playfield.prototype.removeRowAt = function(y) {
 /**
  * Checks that a row is complete. If a row is undefined, it has no cells,
  * if any cells are undefined they are empty
+ * @return bool
  */
 Playfield.prototype.rowComplete = function(y) {
     var complete,
-        i, iMax;
+        i,
+        iMax;
 
     if (typeof this.grid[y] === 'undefined') {
         complete = false;
@@ -82,7 +112,25 @@ Playfield.prototype.rowComplete = function(y) {
 };
 
 /**
+ * Returns array of y coordinates for all completed rows
+ * TODO: pass in optional rows to check (i.e. only check the rows that a newly placed tetromino is touching)
+ */
+Playfield.prototype.getCompletedRows = function() {
+    var self = this,
+        completedRows = [];
+
+    this.traverseRows(function(i, row) {
+        if (self.rowComplete(i)) {
+            completedRows.push(i);
+        }
+    });
+
+    return completedRows;
+};
+
+/**
  * Checks that a cell is empty
+ * @return bool
  */
 Playfield.prototype.cellEmpty = function(x, y) {
     var empty = false;
@@ -98,6 +146,7 @@ Playfield.prototype.cellEmpty = function(x, y) {
 
 /**
  * Checks that a cell is in the bounds of the playfield
+ * @return bool
  */
 Playfield.prototype.cellInBounds = function(x, y) {
     return (y >= 0 && y < this.yCount && x >= 0 && x < this.xCount);
@@ -105,6 +154,7 @@ Playfield.prototype.cellInBounds = function(x, y) {
 
 /**
  * Checks that an array of blocks are valid for placement at their given coordinates
+ * @return bool
  */
 Playfield.prototype.validateBlockPlacement = function(blocks) {
     var valid = true;
@@ -118,10 +168,24 @@ Playfield.prototype.validateBlockPlacement = function(blocks) {
     return valid;
 };
 
+/**
+ * Checks that a block's coordinates are within the bounds of the playfield,
+ * and that the block's cell is available
+ * @param Block block
+ * @return bool
+ */
 Playfield.prototype.validateBlock = function(block) {
-    return (this.cellInBounds(block.x, block.y) && this.cellEmpty(block.x, block.y));
+    return (
+        block.hasOwnProperty('x') &&
+        block.hasOwnProperty('y') &&
+        this.cellInBounds(block.x, block.y) &&
+        this.cellEmpty(block.x, block.y)
+    );
 };
 
+/**
+ * Adds a block to the playfield at the block's coordinates
+ */
 Playfield.prototype.placeBlock = function(block) {
     if (this.validateBlock(block)) {
         if (typeof this.grid[block.y] === 'undefined') {
@@ -133,6 +197,9 @@ Playfield.prototype.placeBlock = function(block) {
     }
 };
 
+/**
+ * Adds an array of blocks to the playfield
+ */
 Playfield.prototype.placeBlocks = function(blocks) {
     for (var i = 0, iMax = blocks.length; i < iMax; i++) {
         this.placeBlock(blocks[i]);
