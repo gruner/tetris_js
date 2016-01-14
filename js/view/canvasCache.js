06387fdc0a42064667cab5e783a0a0204663fe7f
Andrew Gruner <andrew.gruner@gmail.com>
2016-01-14 10:46:08 -0800

    Stub canvasCache methods

--- js/view/canvasCache.js
+++ js/view/canvasCache.js
@@ -1,21 +1,21 @@
-// Caches the parts of the canvas that don't need to be updated on each animation frame
-
-
-// * Create a canvas element
-// * Get its context
-// * Write pixels to that context
-// * Use the canvas in a drawImage() call on another canvas context
-// * (You don't add the cached canvas to the DOM)
-
 'use strict';
 
+/**
+ * Caches the parts of the canvas that don't need to be updated on each animation frame
+ * 
+ * Creates a new canvas element
+ * Get its context
+ * Write pixels to that context
+ * Use the canvas in a drawImage() call on another canvas context
+ * (Don't add the cached canvas to the DOM)
+ */
 module.exports = {
     imageCache: {},
 
     // to retrive a cached image:
-    // otherCtx.drawImage(imageCache[name], 0, 0)
+    // otherCtx.drawImage(imageCache.retrieve(name), 0, 0)
 
-    cache: function(name, image) {
+    addToCache: function(name, image) {
         var canvasCache = document.createElement('canvas'),
             cacheCtx;
 
@@ -25,6 +25,10 @@ module.exports = {
         cacheCtx = canvasCache.getContext('2d');
         cacheCtx.drawImage(image, 0, 0);
 
-        imageCache[name] = canvasCache;
+        this.imageCache[name] = canvasCache;
+    },
+
+    retrive: function(name) {
+        return this.imageCache[name] ? this.imageCache[name] : null;
     }
 };
