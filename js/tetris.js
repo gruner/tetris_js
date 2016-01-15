ac7db1638bfd6a5da93a5f5d713e6a7a9da1410b
Andrew Gruner <andrew.gruner@gmail.com>
2016-01-15 09:53:27 -0800

    Fix cancelAnimationFrame

--- js/tetris.js
+++ js/tetris.js
@@ -2,31 +2,32 @@ var GameEngine = require('./gameEngine'),
     Canvas = require('./view/canvas'),
     events = require('./config/events'),
     eventBinding = require('./eventBinding'),
-    eventDispatcher = require('./eventDispatcher'),
-    debug = require('./debug');
+    eventDispatcher = require('./eventDispatcher');
 
 /**
  * Bootstraps all game components together
  */
 var Tetris = function(canvasElement) {
+    var self = this;
+
     this.gameEngine = new GameEngine();
     this.canvas = new Canvas(canvasElement, this.gameEngine);
     this.frameId = null;
     eventBinding.init();
-    eventDispatcher.subscribe(events.topOut, this.stopLoop);
 
-    var self = this;
 
     // this has to be defined this way or it won't work
     this.run = function() {
-        requestAnimationFrame(self.run);
+        self.frameId = requestAnimationFrame(self.run);
         self.gameEngine.update();
         self.canvas.draw();
     };
-};
 
-Tetris.prototype.stopLoop = function() {
-    window.cancelAnimationFrame(this.frameId);
+    // self.frameId = requestAnimationFrame(self.run);
+
+    eventDispatcher.subscribe(events.topOut, function() {
+        window.cancelAnimationFrame(self.frameId);
+    });
 };
 
 module.exports = Tetris;
