d0f860c27769752b2961d3a4d27a006c604d6e07
Andrew Gruner <andrew.gruner@gmail.com>
2016-01-26 13:33:11 -0800

    Disable sounds that don't have files yet

--- js/config/sounds.js
+++ js/config/sounds.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-    activePieceRotate: 'sounds/activePieceRotate.mp3',
+    //activePieceRotate: 'sounds/activePieceRotate.mp3',
     activePieceLock:   'sounds/activePieceLock.mp3',
-    rowComplete:       'sounds/rowComplete.mp3',
-    invalidMove:       'sounds/invalidMove.mp3',
+    rowComplete:       'sounds/lineClear.mp3',
+    //invalidMove:       'sounds/invalidMove.mp3',
     gameOver:          'sounds/gameOver.mp3'
 };
