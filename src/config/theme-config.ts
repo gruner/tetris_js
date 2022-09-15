e9e4afc76dec301cb681357def4cf2e6df113c26
Andrew Gruner <andrew.gruner@gmail.com>
2022-09-15 15:38:08 -0700

    Add theme config

--- src/config/theme-config.ts
+++ src/config/theme-config.ts
@@ -1,8 +1,20 @@
 import { iTheme } from "../theme/theme";
 
+// RR Colors
+const canton = "#386a6b";
+const citrine = "#777b39";
+const coral = "#cc6633";
+const gold = "#dbb349";
+const graphite = " #727d8c";
+const magnolia = "#e8d5bb";
+const marineBlue = "#2f3747"; // too dark
+const mocha = "#5a3e27";
+const paleLime = "#c3c463";
+const polarBlue = "#7eabb6";
+const shrimpPink = "#e4a473";
+
 /**
  * Config for different themes
- * TODO: Don't use same color for s and z
  */
 export const ThemeConfig: { [index:string] : iTheme } = {
   default: {
@@ -10,12 +22,12 @@ export const ThemeConfig: { [index:string] : iTheme } = {
     ghostPiece: {color: "#333333"},
     tetrominos: {
       i: {color: "#dd5b4d"}, // red
-      o: {color: "#4f718b"}, // blue
+      o: {color: "#4f718b"}, // blue-green
       t: {color: "#4ab18f"}, // green
       j: {color: "purple"},
       l: {color: "#eec857"}, // yellow
       s: {color: "#e07a46"}, // orange
-      z: {color: "#e07a46"}  // orange
+      z: {color: "#7A74A9"}  // blue-grey
     },
     tetrominoBorder: {color: "#000000"}
   },
@@ -26,6 +38,20 @@ ThemeConfig['level0'] = {
 };
 
 ThemeConfig['level1'] = {
+  name: 'Robot Roundup',
+  tetrominos: {
+    i: {color: canton},
+    o: {color: citrine},
+    t: {color: shrimpPink},
+    j: {color: coral},
+    l: {color: graphite},
+    s: {color: paleLime},
+    z: {color: polarBlue}
+  },
+  parent: ThemeConfig.default
+};
+
+ThemeConfig['level2'] = {
   name: 'Global 19.3',
   tetrominos: {
     i: {color: "#C0B988"}, // tan
@@ -39,7 +65,7 @@ ThemeConfig['level1'] = {
   parent: ThemeConfig.default
 };
 
-ThemeConfig['level2'] = {
+ThemeConfig['level3'] = {
   name: 'Global 18.5',
   tetrominos: {
     i: {color: "#02585B"}, // dark green
@@ -50,5 +76,29 @@ ThemeConfig['level2'] = {
     s: {color: "#B3026C"}, // dark pink
     z: {color: "#C57FAA"}  // light pink
   },
+  parent: ThemeConfig.default
+};
+
+ThemeConfig['level4'] = {
+  parent: ThemeConfig.default
+};
+
+ThemeConfig['leve5'] = {
+  parent: ThemeConfig.default
+};
+
+ThemeConfig['level6'] = {
+  parent: ThemeConfig.default
+};
+
+ThemeConfig['level7'] = {
+  parent: ThemeConfig.default
+};
+
+ThemeConfig['level8'] = {
+  parent: ThemeConfig.default
+};
+
+ThemeConfig['level9'] = {
   parent: ThemeConfig.default
 };
